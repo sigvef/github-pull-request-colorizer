58a7efe2b6233dfba0acdead212ecc1173615a5d
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2018-06-06 15:08:44 +0200

    Update content.js

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ const me = document.querySelector('summary[aria-label="View profile and more"] i
   const title = (row.querySelector('.link-gray-dark.h4') || {}).innerText;
   const failsTravis = !!row.querySelector('.commit-build-statuses .text-red');
   const passesTravis = !!row.querySelector('.commit-build-statuses .text-green');
-  const priorityLowLabel = row.querySelector('.labels a[aria-label="Priority: Low"]');
+  const priorityLowLabel = row.querySelector('.labels a[title="Priority: Low"]');
 
 
   let highlight = false;
